dd4f12d827d238f156a406addf800c20f5b485a7
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-11-26 18:27:11 +0530

    ''

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -21,7 +21,7 @@ $(function () {
     const target = parseFloat(element.getAttribute("data-target"));
     let start = 0;
     const stepTime = 10;
-    
+
     // Prevent division by zero if target is 0
     const increment = target > 0 ? target / (duration / stepTime) : 0;
 
@@ -38,9 +38,25 @@ $(function () {
 
   // 4. Run for ALL counters found on the page
   const allCounters = document.querySelectorAll(".stat-number");
-  
+
   allCounters.forEach((counter) => {
     animateCounter(counter);
   });
+
+
+  $('.banner-slider .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    navText: [
+      `<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m15 5l-6 7l6 7"/></svg>`,
+      `<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m9 5l6 7l-6 7"/></svg>`,
+    ],
+  })
 
 });
